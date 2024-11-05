8cb6b852e4524e14828ad507a50cb4b62c5849a6
Yan Marchan <marchan.yan@student.ehu.lt>
2024-11-05 17:04:15 +0200

    Add initial FastAPI setup with user management features

---
+++ backend/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from backend.app.routers import user
+from backend.app.utils.database import engine, Base
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(user.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Gym Assistant API"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("backend.app.main:app", host="localhost", port=8000, reload=True)
